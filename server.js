56f833aaf7f8fccaf88b2c89535213823e1af11c
lakshmiprakasam <30822376+lakshmiprakasam@users.noreply.github.com>
2017-08-11 10:30:40 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -13,11 +13,11 @@ app.get('/article-one',function (req,res) {
     res.send('article one requested and will be served here');
 });
 
-    app.get('/article two',function (req,res) {
+    app.get('/article-two',function (req,res) {
     res.send('article two requested and will be served here');
 });
 
-app.get('/article three',function (req,res) {
+app.get('/article-three',function (req,res) {
     res.send('article three requested and will be served here');
 
 });
